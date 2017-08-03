039f89b125c36004cf4f58656b13a615a7a605d8
rohitudemy <30374847+rohitudemy@users.noreply.github.com>
2017-08-02 17:23:39 -0700

    added profile details

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='stackoverflow',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/rohitudemy/stackoverflow',
+    license='MIT',
+    author='rohitramchandani',
+    author_email='rohitram@mit.edu',
+    description=''
+)
--- topics.py
+++ topics.py
@@ -6,55 +6,55 @@ import requests
 import csv
 import argparse
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 SITE = StackAPI('stackoverflow')
 SITE.key = ""
 SITE.access_token = ""
+TIMESTR = time.strftime("%Y%m%d-%H%M%S")
 
 
 def token():
-
     resp = requests.get('https://stackexchange.com/oauth/dialog?client_id=&scope=no_expiry&'
                         'redirect_uri=https://stackexchange.com/oauth/login_success/')
     pprint(vars(resp))
 
 
-def search():
-
+def search(options):
     ########################################################
-    from_date = '07/01/2017'
-    to_date = '07/27/2017'
-
-    min_answers = 1
-    title_only = False
-    body_only = False
-    topic_only = False
-    nottagged = True
-
-    SITE.page_size = 10
+    from_date = options.from_date
+    to_date = options.to_date
+    SITE.page_size = options.max
     SITE.max_pages = 1
+    min_answers = 1
     ########################################################
 
-    k = open("keywords.csv", "rb")
-    w = open("search.csv", "w+")
+    results = "keyword_results_" + TIMESTR + ".csv"
+    k = open(options.file, "rb")
+    w = open(results, "w+")
 
     kr = csv.reader(k)
     wr = csv.writer(w)
 
     wr.writerow(['keyword', 'question_title', 'closed_date', 'tags', 'answer_id',
                  'answer_user_id', 'answer_user_name', 'answer_user_profile_url', 'answer_user_reputation',
-                 'answer_score'])
+                 'answer_score', 'age', 'location', 'website_url'])
 
     fromdate = int(time.mktime(datetime.datetime.strptime(from_date, '%m/%d/%Y').timetuple()))
     todate = int(time.mktime(datetime.datetime.strptime(to_date, '%m/%d/%Y').timetuple()))
 
+    next(kr, None)
     for row in kr:
 
-        keyword = ''.join(row)
+        keyword = row[0]
+        tagged = row[1]
+        title = row[2]
+        body = row[3]
+
         response = SITE.fetch('search/advanced', fromdate=fromdate, todate=todate, accepted=True, closed=True,
-                              answers=min_answers, nottagged=True, sort='votes', q=keyword)
+                              answers=min_answers, sort='votes', q=keyword, title=title, body=body, tagged=tagged)
 
         for questions in response['items']:
             question_title = questions['title']
@@ -71,28 +71,46 @@ def search():
                 answer_user_reputation = answer['owner']['reputation']
                 answer_score = answer['score']
 
-                wr.writerow([keyword, question_title, closed_date, tags, answer_id, answer_user_id, answer_user_name,
-                             answer_user_profile_url, answer_user_reputation, answer_score])
+                user_data = SITE.fetch('users/' + str(answer_user_id) + '/')
 
+                for data in user_data['items']:
+                    try:
+                        age = data['age']
+                    except KeyError:
+                        age = 'n/a'
+                    try:
+                        location = data['location']
+                    except KeyError:
+                        location = 'n/a'
+
+                    try:
+                        website_url = data['website_url']
+                    except KeyError:
+                        website_url = 'n/a'
+
+                wr.writerow([keyword, question_title, closed_date, tags, answer_id, answer_user_id, answer_user_name,
+                             answer_user_profile_url, answer_user_reputation, answer_score, age, location, website_url])
 
     k.close()
     w.close()
 
-def topics():
 
+def topics(options):
     ########################################################
-    SITE.page_size = 20
+    SITE.page_size = options.max
     SITE.max_pages = 1
-    all_time = False
+    all_time = options.all_time
     ########################################################
 
-    t = open("topics.csv", "rb")
-    u = open("users.csv", "w+")
+    results = "topic_results_" + TIMESTR + ".csv"
+    t = open(options.file, "rb")
+    u = open(results, "w+")
 
     tr = csv.reader(t)
     ur = csv.writer(u)
 
-    ur.writerow(['topic', 'user_id', 'display_name', 'profile_url', 'reputation', 'accept_rate', 'post_count', 'score'])
+    ur.writerow(['topic', 'user_id', 'display_name', 'profile_url', 'reputation', 'accept_rate', 'post_count',
+                 'score', 'age', 'location', 'website_url'])
 
     if all_time:
         time_frame = 'all_time'
@@ -119,10 +137,29 @@ def topics():
             except KeyError:
                 reputation = 'n/a'
 
-            ur.writerow([topic, user_id, display_name, profile_url, reputation, accept_rate, post_count, score])
+            user_data = SITE.fetch('users/' + str(user_id) + '/')
+
+            for data in user_data['items']:
+                try:
+                    age = data['age']
+                except KeyError:
+                    age = 'n/a'
+                try:
+                    location = data['location']
+                except KeyError:
+                    location = 'n/a'
+
+                try:
+                    website_url = data['website_url']
+                except KeyError:
+                    website_url = 'n/a'
+
+            ur.writerow([topic, user_id, display_name, profile_url, reputation, accept_rate, post_count,
+                             score, age, location, website_url])
 
     t.close()
     u.close()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -130,8 +167,13 @@ if __name__ == '__main__':
                     'search': search}
 
     parser.add_argument('command', choices=FUNCTION_MAP.keys())
+    parser.add_argument("--file", help="Input CSV name", default="topics.csv")
+    parser.add_argument("--from_date", help="Keyword Search Only: From Date", default="01/01/2017")
+    parser.add_argument("--to_date", help="Keyword Search Only: To Date", default=time.strftime("%m/%d/%Y"))
+    parser.add_argument("--all_time", help="Topic Search Only: All Time", default=False)
+    parser.add_argument("--max", help="Max Results", default=10)
 
     args = parser.parse_args()
 
     func = FUNCTION_MAP[args.command]
-    func()
+    func(args)
